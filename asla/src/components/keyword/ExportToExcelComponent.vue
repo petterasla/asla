d183255d18ccffffb5864942efdb2b38ab297adc
Petter Asla <petter.asla@gmail.com>
2022-02-10 20:14:54 +0100

    js libray for downloading excel file

--- asla/src/components/keyword/ExportToExcelComponent.vue
+++ asla/src/components/keyword/ExportToExcelComponent.vue
@@ -1,19 +1,32 @@
 <template>
     <div class="export-to-excel">
         <h1> 3 </h1>
-        <button>Export to excel</button>
+        <button @click=downloadExcelFile>Export to excel</button>
     </div>
 </template>
 
 
 <script>
+import XLSX from 'xlsx';
+
 export default {
-    name: "ExportToExcelComponent"
+    name: "ExportToExcelComponent",
+    methods: {
+        downloadExcelFile: function() {
+            const data = XLSX.utils.json_to_sheet(this.items)
+            const wb = XLSX.utils.book_new()
+            XLSX.utils.book_append_sheet(wb, data, 'data')
+            XLSX.writeFile(wb,'demo.xlsx')
+        }
+    }
 }
+
+
 </script>
 
 <style scoped>
 .export-to-excel {
-    margin-top: 3em
+    margin-top: 3em;
+    margin-bottom: 5em;
 }
 </style>
