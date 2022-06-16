590ea5db72c4b310d77ec2768b291ac0a7e634d2
Petter Asla <petter.asla@gmail.com>
2022-06-16 10:22:19 +0200

    Adding google login component

--- asla/src/main.ts
+++ asla/src/main.ts
@@ -4,12 +4,16 @@ import router from "./router";
 import store from "./store";
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import vue3GoogleLogin from 'vue3-google-login'
 
 
 createApp(App)
     .use(router)
     .use(store)
     .use(ElementPlus)
+    .use(vue3GoogleLogin, {
+        clientId: '749862802460-aeuuju8u5m1l6s12bb0671ard7g216vs.apps.googleusercontent.com'
+    })
     .mount("#app");
 
 //app.config.globalProperties.$store=store;
