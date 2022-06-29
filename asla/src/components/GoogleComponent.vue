3a241da930e628cdc904103f03849dc14193311a
Petter Asla <petter.asla@gmail.com>
2022-06-29 14:13:18 +0200

    login button to google with auth code

--- asla/src/components/GoogleComponent.vue
+++ asla/src/components/GoogleComponent.vue
@@ -1,7 +1,7 @@
 
 <template>
     <div class="center-button">
-        <div id="signin_button"></div>
+        <button id="signin_button" @click="handleSomething" >Authorize with Google</button>
         <!--
         <div id="g_id_onload"
             data-client_id="749862802460-aeuuju8u5m1l6s12bb0671ard7g216vs.apps.googleusercontent.com"
@@ -39,6 +39,58 @@ export default {
                 console.log("User failed to log in")
                 this.$store.dispatch("updateAuthentication", false);
             }
+        },
+        handleSomething() {
+
+
+//        window.addEventListener('load', () => {
+           const googleId = process.env.VUE_APP_GOOGLE_CLIENT_ID;
+           const client = window.google.accounts.oauth2.initCodeClient({
+            client_id: googleId,
+            scope: "https://www.googleapis.com/auth/adwords",
+            ux_mode: 'popup',
+            callback: (response) => {
+                console.log("Inside google client callback ")
+                console.log(response)
+                console.log("code: " + response.code)
+                const xhr = new XMLHttpRequest();
+                xhr.open('POST', 'http://localhost:8080/api/login', true);
+                xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                // Set custom header for CRSF
+                xhr.setRequestHeader('X-Requested-With', 'XmlHttpRequest');
+                xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+                xhr.setRequestHeader('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+                xhr.setRequestHeader('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+                xhr.onload = function() {
+                    console.log('Auth code response: ' + response);
+                };
+                xhr.send();
+                console.log("finished google client callback")
+                console.log(response)
+            }
+  //        });
+    
+          //const loginButton = document.getElementById('signin_button');
+          //loginButton.setAttribute('onclick', console.log("button clicked"));
+                // console.log(window.google);
+                // const googleId = process.env.VUE_APP_GOOGLE_CLIENT_ID;
+                // console.log("google Id: " + googleId)
+                // window.google.accounts.id.initialize({
+                //     client_id: process.env.VUE_APP_GOOGLE_CLIENT_ID,
+                //     callback: this.handleCredentialResponse
+                // });
+                // window.google.accounts.id.renderButton(
+                //     document.getElementById("signin_button"),
+                //     { theme: "outline", size: "large" }  // customization attributes
+                // );
+        });
+
+        // const loginButton = document.getElementById('signin_button');
+        // loginButton.onclick = this.handleSomething(client);
+            console.log("calling requestCode");
+            var response = client.requestCode();
+            console.log("finished requesting code")
+            console.log(window.google);
         }
     },
     setup() {
@@ -49,24 +101,9 @@ export default {
         }
     },
     mounted() {
-      let gapiScript = document.createElement('script')
-      gapiScript.setAttribute('src', 'https://accounts.google.com/gsi/client')
-      document.head.appendChild(gapiScript)
-
-       window.addEventListener('load', () => {
-                console.log(window.google);
-                const googleId = process.env.VUE_APP_GOOGLE_CLIENT_ID;
-                console.log("google Id: " + googleId)
-                window.google.accounts.id.initialize({
-                    client_id: process.env.VUE_APP_GOOGLE_CLIENT_ID,
-                    callback: this.handleCredentialResponse
-                });
-                window.google.accounts.id.renderButton(
-                    document.getElementById("signin_button"),
-                    { theme: "outline", size: "large" }  // customization attributes
-                );
-            })
-
+        let gapiScript = document.createElement('script')
+        gapiScript.setAttribute('src', 'https://accounts.google.com/gsi/client')
+        document.head.appendChild(gapiScript);
     }
 }
 </script>
