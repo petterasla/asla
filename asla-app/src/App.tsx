e75183183490f40c1be9959f5fc520b443a0a206
Petter Asla <petterasla@Petter-sin-MacBook-Pro.local>
2024-01-04 13:18:07 +0100

    rewrite front page header to using image instead

--- asla-app/src/App.tsx
+++ asla-app/src/App.tsx
@@ -6,25 +6,7 @@ function App() {
   return (
     <div className="App">
         <div className="App-header">
-          <div style={ {"--i": 1} as HeaderVariableCSS } >A</div>
-          <div style={ {"--i": 2} as HeaderVariableCSS } >s</div>
-          <div style={ {"--i": 3} as HeaderVariableCSS } >l</div>
-          <div style={ {"--i": 4} as HeaderVariableCSS } >a</div>
-          <div className="invisible-space" >-</div>
-          <div style={ {"--i": 6} as HeaderVariableCSS } >E</div>
-          <div style={ {"--i": 7} as HeaderVariableCSS } >n</div>
-          <div style={ {"--i": 8} as HeaderVariableCSS } >g</div>
-          <div style={ {"--i": 9} as HeaderVariableCSS } >i</div>
-          <div style={ {"--i": 10} as HeaderVariableCSS } >n</div>
-          <div style={ {"--i": 11} as HeaderVariableCSS } >e</div>
-          <div style={ {"--i": 12} as HeaderVariableCSS } >e</div>
-          <div style={ {"--i": 13} as HeaderVariableCSS } >r</div>
-          <div style={ {"--i": 14} as HeaderVariableCSS } >i</div>
-          <div style={ {"--i": 15} as HeaderVariableCSS } >n</div>
-          <div style={ {"--i": 16} as HeaderVariableCSS } >g</div>
-          <div className="invisible-space" >-</div>
-          <div style={ {"--i": 18} as HeaderVariableCSS } >A</div>
-          <div style={ {"--i": 19} as HeaderVariableCSS } >S</div>
+          <img src={"asla-engineering-high-resolution-logo-white-transparent.png"} alt={"image"}/>
         </div>
     </div>
   );
