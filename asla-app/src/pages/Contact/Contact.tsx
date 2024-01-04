ac5b707bdf189ff068c80a3391a61b316cb9d165
Petter Asla <petterasla@Petter-sin-MacBook-Pro.local>
2024-01-04 15:58:21 +0100

    Contact me info up

--- asla-app/src/pages/Contact/Contact.tsx
+++ asla-app/src/pages/Contact/Contact.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import './contact.css'
 
 const Contact = () => {
-    return <div>Kontaktinfo</div>
+    return <div className="contact">
+                <div className="row">
+                    Petter Fagerlund Asla
+                </div>
+                <div className="row">
+                    jobb.petter.asla@gmail.com
+                </div>
+                <div className="row">
+                    913 75 468
+                </div>
+                <div className="row">
+                    <a href="https://www.linkedin.com/in/petter-fagerlund-asla-77970359/">LinkedIn</a>
+                </div>
+            </div>
 }
 
 export default Contact
