1d19bab70d2c7e642b75c504a21885dc31b650f6
Petter Asla <petterasla@Petter-sin-MacBook-Pro.local>
2024-01-05 07:21:48 +0100

    update contact using contact card template

--- asla-app/src/pages/Contact/Contact.tsx
+++ asla-app/src/pages/Contact/Contact.tsx
@@ -1,21 +1,25 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faFacebook} from "@fortawesome/free-brands-svg-icons";
 import './contact.css'
 
 const Contact = () => {
     return <div className="contact">
-                <div className="row">
-                    Petter Fagerlund Asla
-                </div>
-                <div className="row">
-                    jobb.petter.asla@gmail.com
-                </div>
-                <div className="row">
-                    913 75 468
-                </div>
-                <div className="row">
-                    <a href="https://www.linkedin.com/in/petter-fagerlund-asla-77970359/">LinkedIn</a>
+            <div className="contact-card">
+                <img src={"Petter.png"} alt="Petter" />
+                <p className="name">Petter F. Asla</p>
+                <p className="title">Daglig leder</p>
+                <p className="title-company">Asla Engineering AS</p>
+                <p className="email">jobb.petter.asla@gmail.com </p>
+                <p className="phone">913 75 468</p>
+                <div className={"contact-logo"} >
+                    <a href="#"><i className="fa fa-dribbble"></i></a>
+                    {/*<a href=""><i className="fa fa-twitter"></i></a>*/}
+                    <a href="https://www.linkedin.com/in/petter-fagerlund-asla-77970359/"><FontAwesomeIcon icon={faLinkedin} /></a>
+                    <a href="https://www.facebook.com/petter.asla"><FontAwesomeIcon icon={faFacebook} /></a>
                 </div>
             </div>
+    </div>
 }
 
 export default Contact
