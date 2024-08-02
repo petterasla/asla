3011234d4588dd5ed2cf36436bd507d6862f0fec
petterasla <jobb.petter.asla@gmail.com>
2024-08-02 13:35:58 +0200

    Update CV info about 2024

--- asla-app/src/pages/Background/Background.tsx
+++ asla-app/src/pages/Background/Background.tsx
@@ -31,14 +31,15 @@ const Background = () => {
             <div className="experience-description CV-item">
                 <ul>
                     <li>
-                        Utvikle og vedlikeholde kjernesystemet for vare, kunde og leverandør i NorgesGruppen
-                    </li>
-                    <li>
-                        Tilpasse kjernesystemet for overgang til nytt kjernesystem
+                        Prosjekt Dovre: Tilpasse kjernesystemet for vare, kunde og leverandør i NorgesGruppen
+                        til å gå fra master av data til slave av nytt kjernesystem. Prosjektet stiller store krav til
+                        domenekunnskap, samt ferdigheter til å omstille seg raskt. Petter tiltrådte som senior utvikler,
+                        med tett oppfølging mot forretning for å håndtere deres krav.
                     </li>
                 </ul>
             </div>
-            <div className="experience-summary CV-item">Java EE, Oracle SQL, Weblogic, Webhook, JMS, EJB, Hibernate
+            <div className="experience-summary CV-item"> Azure DevOps and Pipeline, Azure Function, Azure Bicep (IaC),
+                Java EE, Oracle SQL, Weblogic, Webhook, JMS, EJB, Hibernate
             </div>
         </div>
         <div className="CV-bulk flex-item">
@@ -47,17 +48,17 @@ const Background = () => {
             <div className="experience-description CV-item">
                 <ul>
                     <li>
-                        Automatisere fakturautsending etter fullført kundebehandling gjennom en integrasjon mellom
-                        bookingsystem og regnskapssystem
+                        Oppstartsbedrift: Automatisere fakturautsending etter fullført kundebehandling gjennom en integrasjon mellom
+                        bookingsystem og regnskapssystem. Petter fullautomatiserte dette med Github Actions og skyleverandøren AWS.
                     </li>
                 </ul>
             </div>
-            <div className="experience-summary CV-item">Trafft bookingsystem, PowerOffice, Webhooks, OpenAPI, Github Action, Pulumi (IaC),
+            <div className="experience-summary CV-item"> Trafft bookingsystem, PowerOffice, Webhooks, OpenAPI, Github Action, Pulumi (IaC),
                 AWS (ApiGateway, Lambda, Route53, System Manager, EventBridge, IAM, CloudWatch), Python
             </div>
         </div>
         <div className="CV-bulk flex-item">
-            <div className="experience-header CV-item">Norsk Tipping</div>
+            <div className="experience-header CV-item">Norsk Tipping - Ansatt</div>
             <div className="experience-date CV-item">2021.08 - 2023.12</div>
             <div className="experience-description CV-item">
                 <ul>
@@ -82,6 +83,15 @@ const Background = () => {
         <div className="CV-bulk flex-item">
             <div className="experience-header CV-item">Netcompany - OBOS IT</div>
             <div className="experience-date CV-item">2019.08 - 2021.08</div>
+            <div className="experience-description CV-item">
+                <ul>
+                    <li>
+                        API utvikling: Pakke inn eldre database-først APIer fra stormaskiner i REST APIer og tilgjengeliggjøre
+                        disse for interne og eksterne kunder i organisasjonen. Petter sin rolle var senior utvikler,
+                        ansvarlig for å være med i hele prosessen fra analyse av kundebehov, til utvikling og levering av produkt til kunde.
+                    </li>
+                </ul>
+            </div>
             <div className="experience-summary CV-item">Seniorkonsulent, Java/Kotlin, mikrotjenester, Jenkins, REST,
                 SQL,
                 Confluence, Jira
@@ -90,6 +100,15 @@ const Background = () => {
         <div className="CV-bulk flex-item">
             <div className="experience-header CV-item">Netcompany - NorgesGruppen Data</div>
             <div className="experience-date CV-item">2017.04 - 2018.12</div>
+            <div className="experience-description CV-item">
+                <ul>
+                    <li>
+                        Videreutvikling av NorgesGruppens eget økonomi og avstemmingssystem kalt Felles Avregning, skrevet
+                        i Java EE backend og Java Swing frontend. Petter startet også fornyelse av frontend til en webløsning
+                        skrevet i React/Redux. Oppdraget krevde god domenekunnskap og tett kommunikasjon med forretning ved ny og eksisterende funksjonalitet.
+                    </li>
+                </ul>
+            </div>
             <div className="experience-summary CV-item">Seniorkonsulent, Java EE, Hibernate, Bamboo, Confluence, Jira,
                 SQL,
                 WebLogic React/Redux
