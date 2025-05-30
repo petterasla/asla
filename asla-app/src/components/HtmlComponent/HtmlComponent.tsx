123b9999f4c7826bb1eb2ffa3a9a3c055caa3af4
petterasla <jobb.petter.asla@gmail.com>
2025-05-30 10:24:15 +0200

    Legge til skråstrek for referanse til html. Fungerer lokalt men ikke online

--- asla-app/src/components/HtmlComponent/HtmlComponent.tsx
+++ asla-app/src/components/HtmlComponent/HtmlComponent.tsx
@@ -2,7 +2,7 @@ const HtmlComponent = () => {
     return (
         <div style={{ width: '100%', height: '100vh' }}>
             <iframe
-                src="stitch-ui.html"
+                src="/stitch-ui.html"
                 title="Stitch Google Generated Site"
                 width="100%"
                 height="100%"
